4f838442ec15641086300c45f74ec2e3f3601e2b
Francois-Pierre Leblanc <Francois-Pierre.Leblanc@shawdirect.ca>
2019-08-09 16:46:53 -0400

    Added a default BS4 Card template.

--- summernote-ext-addclass.js
+++ summernote-ext-addclass.js
@@ -36,7 +36,7 @@
                 //  console.log("Please define your summernote.options.addclass.classTags array");
             }
             if (typeof context.options.addclass.htmlTemplates === 'undefined') {
-                context.options.addclass.htmlTemplates = [];
+                context.options.addclass.htmlTemplates = [{title: 'Card', before:'<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">Card title</h5><h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6><p class="card-text">', defaultContent:'Some quick example text to build on the card title and make up the bulk of the card\'s content.', after:'</p><a href="#" class="card-link">Card link</a></div></div>',}];
             }
             if (typeof context.options.addclass.icon === 'undefined') {
                 context.options.addclass.icon = 'class="fa fa-css3"'
